e1fd01a40c00b5b4932110cfae81113acc2b88d1
MBEJTA <michel_bejta@live.nl>
2018-02-25 21:21:02 +0100

    lesson 3

--- app/src/main/java/com/example/android/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/android/swoosh/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.android.swoosh
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // launch next activity
+        getStartedButton.setOnClickListener() {
+            // intent is a object that lets you interact with other applications, as well as your own
+            // example = intent (action) open camera app and take a picture / navigate to another activity. (implicit / explicit intents)
+
+            val leagueIntent = Intent (this, SecondActivity::class.java)
+            startActivity(leagueIntent)
+
+        }
     }
 }
